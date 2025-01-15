aa7fff6c143c2a8d0343ddfc5b76eb103b93ceb9
Ishinaru <dvplima.cic@uesc.br>
2025-01-15 08:57:23 -0300

    Iniciando testes unitarios classe Calendario

--- CalendarioAcad/API/Services/Calendarios/ICalendarioInterface.cs
+++ CalendarioAcad/API/Services/Calendarios/ICalendarioInterface.cs
@@ -7,10 +7,10 @@ namespace API.Services.Calendarios
     public interface ICalendarioInterface
     {
         Task<ResponseModel<Calendario>> AprovarCalendario(int idCalendario);
-        Task<ResponseModel<List<Calendario>>> ListarCalendarios(); 
-        Task<ResponseModel<Calendario>> CalendarioPorId(int idCalendario);
         Task<ResponseModel<List<Calendario>>> CriarCalendario(CriarCalendarioDTO criarCalendarioDTO);
         Task<ResponseModel<Calendario>> DesativarCalendario(int idCalendario);
+        Task<ResponseModel<List<Calendario>>> ListarCalendarios(); 
+        Task<ResponseModel<Calendario>> CalendarioPorId(int idCalendario);
         Task<string> GerarNumeroResolucao(int ano);
 
     }
--- CalendarioAcad/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ CalendarioAcad/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6794e3ee13f1fa30d7959a975475b97a9c987bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
