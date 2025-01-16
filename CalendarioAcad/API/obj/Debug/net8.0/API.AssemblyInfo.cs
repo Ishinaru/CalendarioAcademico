beb2ab9df0d56cccb664fc5c4e7f00b1b6107179
Ishinaru <dvplima.cic@uesc>
2025-01-15 22:24:12 -0300

    modificação gitignore

--- CalendarioAcad/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ CalendarioAcad/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06b52b23248ea475b9ecd43746958ab98495cb67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042a0bbea5dba5fe8cf45dc1335d1af96e5018bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
