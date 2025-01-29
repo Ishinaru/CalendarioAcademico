28cda885ad4321e268eac1bdac1aa19b0e342f87
Ishinaru <dvplima.cic@uesc.br>
2025-01-29 11:59:03 -0300

    add GetEventoById no eventoService do frontend

--- CalendarioAcad/BlazorApp/Services/EventoService.cs
+++ CalendarioAcad/BlazorApp/Services/EventoService.cs
@@ -11,5 +11,11 @@ namespace BlazorApp.Services
             var response = await client.GetFromJsonAsync<ResponseModel<List<Evento>>>($"/api/Eventos/EventoPorCalendario/{idCalendario}");
             return response?.Dados ?? [];
         }
-    }
+
+        public async Task<Evento> GetEventoById(int idEvento)
+        {
+            var client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+            var response = await client.GetFromJsonAsync<ResponseModel<Evento>>($"/api/Eventos/EventoPorId/{idEvento}");
+            return response?.Dados;
+        }
 }
