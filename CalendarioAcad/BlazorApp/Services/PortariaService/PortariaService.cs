b083dbbe22c0e9c3596d3b22a25390b4deaafce1
Ishinaru <dvplima.cic@uesc.br>
2025-02-05 08:57:55 -0300

    Add GetCalendariosAprovados em API-Services

--- CalendarioAcad/BlazorApp/Services/PortariaService/PortariaService.cs
+++ CalendarioAcad/BlazorApp/Services/PortariaService/PortariaService.cs
@@ -4,19 +4,8 @@ using System.Net.Http.Json;
 
 namespace BlazorApp.Services.PortariaService
 {
-    public class PortariaService(IHttpClientFactory httpClient)
+    public class PortariaService : IPortariaInterface
     {
-        public async Task<ResponseModel<Portaria>> CriarPortariaAsync(CriarPortariaEventosDTO dados)
-        {
-            var client = httpClient.CreateClient(Configuration.HttpClientName);
-            var response = await client.PostAsJsonAsync($"/api/Portaria/CriarPortaria", dados);
-            if (!response.IsSuccessStatusCode)
-            {
-                var errorResponse = await response.Content.ReadFromJsonAsync<ResponseModel<Portaria>>();
-                throw new Exception(errorResponse?.Mensagem);
-            }
-
-            return await response.Content.ReadFromJsonAsync<ResponseModel<Portaria>>();
-        }
+       
     }
 }
